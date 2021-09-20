3a25f22bfef922f036e161780f8e9e65fee4f60d
paul <423357+toothbrush@users.noreply.github.com>
2021-09-20 22:23:45 +1000

    Pretty-print function keys.

--- SeeKee/Keystroke.swift
+++ SeeKee/Keystroke.swift
@@ -18,13 +18,16 @@ struct Keystroke {
 }
 
 extension Keystroke: CustomStringConvertible {
+
+    // More on that shifting logic..  Probably if any other modifiers are pressed, then we should show shift regardless? No.  See C-x # in Emacs.  Only if we have a special keykap should we show shift.
     var description: String {
         var display = ""
         display.append(self.ctrl ? "Ctrl-" : "")
         display.append(self.alt ? "Alt-" : "")
         display.append(self.cmd ? "Cmd-" : "")
-        display.append(self.shift ? "shift-" : "")
-        display.append(self.cap)
+        // TODO: we need fancier logic around whether we should display shift or not.  E.g., if it's "~" we should not display shift, but if it's an arrow key we probably should.
+        // display.append(self.shift ? "shift-" : "")
+        display.append(self.original_event.keyCap())
         return display
     }
 }
---
+++ SeeKee/NSEventExtensions.swift
@@ -0,0 +1,65 @@
+//
+//  NSEventExtensions.swift
+//  SeeKee
+//
+//  Created by Paul on 20/9/21.
+//
+
+import Carbon
+import Cocoa
+
+extension NSEvent {
+    func keyCap() -> String {
+        switch Int(self.keyCode) {
+            case kVK_Escape:
+                return "Esc"
+            case kVK_F1:
+                return "F1"
+            case kVK_F2:
+                return "F2"
+            case kVK_F3:
+                return "F3"
+            case kVK_F4:
+                return "F4"
+            case kVK_F5:
+                return "F5"
+            case kVK_F6:
+                return "F6"
+            case kVK_F7:
+                return "F7"
+            case kVK_F8:
+                return "F8"
+            case kVK_F9:
+                return "F9"
+            case kVK_F10:
+                return "F10"
+            case kVK_F11:
+                return "F11"
+            case kVK_F12:
+                return "F12"
+            case kVK_Delete:
+                return "BkSpc"
+            case kVK_ForwardDelete:
+                return "Del"
+            case kVK_Tab:
+                return "TAB"
+            case kVK_Return:
+                return "RET"
+            case kVK_Space:
+                return "Space"
+            case kVK_LeftArrow:
+                return "Left"
+            case kVK_RightArrow:
+                return "Right"
+            case kVK_UpArrow:
+                return "Up"
+            case kVK_DownArrow:
+                return "Down"
+            default:
+                if let char = self.charactersIgnoringModifiers {
+                    return char
+                }
+                return "??"
+        }
+    }
+}
