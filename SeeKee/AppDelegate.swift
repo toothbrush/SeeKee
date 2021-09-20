d6d84d054cd6724eff86cf3c891b9040b3a37459
paul <423357+toothbrush@users.noreply.github.com>
2021-09-20 21:32:06 +1000

    POC keyHandler

--- SeeKee/AppDelegate.swift
+++ SeeKee/AppDelegate.swift
@@ -6,15 +6,33 @@
 //
 
 import Cocoa
+import Carbon
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
 
+    let keycode = UInt16(kVK_ANSI_X)
+    let keymask: NSEvent.ModifierFlags = [.command, .control]
+
+    func handler(event: NSEvent!) {
+        if event.keyCode == self.keycode
+            && event.modifierFlags.contains(keymask) {
+            print("PRESSED")
+        }
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let opts = NSDictionary(object: kCFBooleanTrue, forKey: kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString) as CFDictionary
+
+        guard AXIsProcessTrustedWithOptions(opts) == true
+        else {
+            print("scheisse")
+            return
+        }
+
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown, handler: self.handler)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
