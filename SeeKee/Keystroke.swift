4647a4f86b0565e12593a4192a03cbe290bd19b1
paul <423357+toothbrush@users.noreply.github.com>
2021-09-22 15:34:56 +1000

    Slightly more subtle logic on whether to show Shift-.

--- SeeKee/Keystroke.swift
+++ SeeKee/Keystroke.swift
@@ -21,12 +21,21 @@ extension Keystroke: CustomStringConvertible {
 
     // More on that shifting logic..  Probably if any other modifiers are pressed, then we should show shift regardless? No.  See C-x # in Emacs.  Only if we have a special keykap should we show shift.
     var description: String {
+        let modsWeCareAbout = self.original_event
+            .modifierFlags
+            .intersection([
+                .command,
+                .control,
+                .option,
+                .shift,
+                .function, // Useful for arrow keys
+            ])
+        let modsOtherThanShift = modsWeCareAbout.isStrictSuperset(of: .shift)
         var display = ""
         display.append(self.cmd ? "Cmd-" : "")
         display.append(self.ctrl ? "Ctrl-" : "")
         display.append(self.alt ? "Alt-" : "")
-        // TODO: we need fancier logic around whether we should display shift or not.  E.g., if it's "~" we should not display shift, but if it's an arrow key we probably should.
-        display.append(self.shift && self.original_event.modifierFlags.contains(.function) ? "Shift-" : "")
+        display.append(self.shift && modsOtherThanShift ? "Shift-" : "")
         display.append(self.original_event.keyCap())
         return display
     }
